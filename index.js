53683205d59e4881fc6f2caa157c4a1152bd8986
kimenyu <njorogekimenyu@gmail.com>
2024-04-06 13:28:22 +0300

    adding a callback logic

--- index.js
+++ index.js
@@ -103,48 +103,16 @@ app.post ("/stk", generateToken, async(req, res) => {
     })
 })
 
+app.post('/callback', (req, res) => {
+    const callbackData = req.body;
+  
+    // Log the callback data to the console
+    console.log(callbackData.Body);
+  
+    if(!callbackData.Body.stkCallback.CallbackMetadata) {
+        console.log(callbackData.Body);
+        return res.json('ok');
+    }
 
-app.post('/callback', async (req, res) => {
-    try{
-
-       
-            //callback details
-    
-            const {
-                MerchantRequestID,
-                CheckoutRequestID,
-                ResultCode,
-                ResultDesc,
-                CallbackMetadata
-                     }   = req.body.Body.stkCallback
-    
-        //     get the meta data from the meta
-            const meta = Object.values(await CallbackMetadata.Item)
-            const PhoneNumber = meta.find(o => o.Name === 'PhoneNumber').Value.toString()
-            const Amount = meta.find(o => o.Name === 'Amount').Value.toString()
-            const MpesaReceiptNumber = meta.find(o => o.Name === 'MpesaReceiptNumber').Value.toString()
-            const TransactionDate = meta.find(o => o.Name === 'TransactionDate').Value.toString()
-    
-            // do something with the data
-            console.log("-".repeat(20)," OUTPUT IN THE CALLBACK ", "-".repeat(20))
-            console.log(`
-                MerchantRequestID : ${MerchantRequestID},
-                CheckoutRequestID: ${CheckoutRequestID},
-                ResultCode: ${ResultCode},
-                ResultDesc: ${ResultDesc},
-                PhoneNumber : ${PhoneNumber},
-                Amount: ${Amount}, 
-                MpesaReceiptNumber: ${MpesaReceiptNumber},
-                TransactionDate : ${TransactionDate}
-            `)
-    
-            res.json(true)
-    
-        }catch (e) {
-            console.error("Error while trying to update LipaNaMpesa details from the callback",e)
-            res.status(503).send({
-                message:"Something went wrong with the callback",
-                error : e.message
-            })
-        }    
-});
+    console.log(callbackData.Body.stkCallback.CallbackMetadata);
+  });
